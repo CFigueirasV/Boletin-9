26857a5b7ee0f26cc8446467582e079274cb2ed5
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-19 10:41:01 +0100

    Boletin9

--- Boletin9_2/src/boletin9_2/Boletin9_2.java
+++ Boletin9_2/src/boletin9_2/Boletin9_2.java
@@ -10,9 +10,9 @@ public class Boletin9_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    SumaProducto repo = new SumaProducto();
-        System.out.println("Suma: " + repo.suma() + "\nProducto: " 
-                + repo.producto());
+    SumaProducto roto = new SumaProducto();
+        System.out.println("Suma: " + roto.suma() + "\nProducto: " 
+                + roto.producto());
     }
     
 }
