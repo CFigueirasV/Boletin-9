e04f1fe396c80c21fefc9aecdb155ccc5ad7fc08
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-19 10:35:54 +0100

    Boletin9

---
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,17 @@
+package boletin9_5;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ Serie coche = new Serie();
+        coche.serieSuma();
+    }
+    
+}
