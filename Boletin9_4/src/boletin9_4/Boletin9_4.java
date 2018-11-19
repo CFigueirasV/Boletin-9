c5e528a922a42a17ec892a4bfac1ce52d64d70af
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-19 10:27:44 +0100

    Boletin9

---
+++ Boletin9_4/src/boletin9_4/Boletin9_4.java
@@ -0,0 +1,17 @@
+package boletin9_4;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ Tabla roja = new Tabla();
+        roja.tablaMultiplicar();
+    }
+    
+}
