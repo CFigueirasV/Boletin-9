be82e805a70b41277ce626263db075f8dda75f49
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-19 10:44:28 +0100

    Boletin9

---
+++ Boletin9_6/src/boletin9_6/Boletin9_6.java
@@ -0,0 +1,17 @@
+package boletin9_6;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin9_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Sueldos rojo = new Sueldos();
+        rojo.lecturaSueldos();
+    }
+    
+}
